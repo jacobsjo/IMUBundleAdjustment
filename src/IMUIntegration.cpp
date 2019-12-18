b3047367b57772999e7d9253931599041f45cd01
Jochen Jacobs <JochenJacobs33@gmail.com>
2019-12-18 11:24:58 +0100

    IMUIntegration header file

--- src/IMUIntegration.cpp
+++ src/IMUIntegration.cpp
@@ -1,1 +1,25 @@
 #include "IMUIntegration.h"
+
+IMUIntegration::IMUIntegration() {
+
+}
+
+void IMUIntegration::addImuStep(Vector3 deltaPosition, Vector3 deltaOrientation) {
+
+}
+
+Vector3 IMUIntegration::getPosition(float time) {
+    return nullptr;
+}
+
+void IMUIntegration::setPosition(float time, Vector3 position) {
+
+}
+
+Vector3 IMUIntegration::getOrientation(float time) {
+    return nullptr;
+}
+
+void IMUIntegration::setOrientation(float time, Vector3 orientation) {
+
+}
--- src/IMUIntegration.h
+++ src/IMUIntegration.h
@@ -3,7 +3,15 @@
 
 
 class IMUIntegration {
+public:
+    IMUIntegration();
+    void addImuStep(Vector3 deltaPosition, Vector3 deltaOrientation);
 
+    Vector3 getPosition(float time);
+    void setPosition(float time, Vector3 position);
+
+    Vector3 getOrientation(float time);
+    void setOrientation(float time, Vector3 orientation);
 };
 
 
