ddbea41cb14045ec4494d0807014dda9dc66f3e8
oushesh <oushesh@gmail.com>
2020-01-24 12:56:34 +0100

    added KeyFrame Index

--- KeyPointDetection/TEST/ORB.cpp
+++ KeyPointDetection/TEST/ORB.cpp
@@ -1,5 +1,5 @@
 //How to build? g++ ORB.cpp -o ORB `pkg-config --cflags --libs opencv`
-//How to call the Function: ./ORB 1.png 2.png
+//How to call the Function: ./ORB ../../data/1.png ../../data/2.png
 #include <iostream>
 #include <opencv2/core/core.hpp>
 #include <opencv2/features2d/features2d.hpp>
@@ -7,7 +7,7 @@
 
 using namespace std;
 using namespace cv;
-int main ( int argc, char** argv )
+int main (int argc, char** argv)
 {
 
     if ( argc != 3 )
@@ -16,12 +16,33 @@ int main ( int argc, char** argv )
         return 1;
     }
 
+
+    //Loop over the data folder build the image file inde
+    //Build a keyFrame name. Each image is a KeyFrame Name
+    vector<cv::String> fn;
+
+    glob("../../data/*.png", fn, false);
+    vector<Mat> images;
+    vector <int> Frames;
+    size_t count = fn.size(); //number of png files in images folder
+    for (size_t i=0;i<count;i++)
+    {
+      Frames.push_back(i);
+
+      cout << "image names:" << fn[i] << endl;
+      cout << "Frame names:" << Frames[i] << endl;
+    }
+
+    /*
+    for (size_t i=0; i<count; i++)
+      images.push_back(imread(fn[i]));
+      cout << "images" << images[i] << endl;
+    */
     Mat img_1 = imread ( argv[1], CV_LOAD_IMAGE_COLOR );
     Mat img_2 = imread ( argv[2], CV_LOAD_IMAGE_COLOR );
 
 
     //Vector Keypoints, keypoints
-
     std::vector<KeyPoint> keypoints_1, keypoints_2;
     Mat descriptors_1, descriptors_2;
     Ptr<FeatureDetector> detector = ORB::create();
@@ -66,7 +87,6 @@ int main ( int argc, char** argv )
     for (vector<DMatch>::size_type i=0;i<good_matches.size();i++)
     {
       cout << keypoints_1[good_matches[i].queryIdx].pt.x << "Query points_x" << "\t" << keypoints_1[good_matches[i].queryIdx].pt.y << "Query POints_Y" << endl;
-
       cout << keypoints_2[good_matches[i].trainIdx].pt.x << "Train points_x" << "\t" << keypoints_2[good_matches[i].trainIdx].pt.y << "Train POints_Y" << endl;
     }
     //TODO: write the keypoints in the format <x>,<y>
@@ -80,9 +100,9 @@ int main ( int argc, char** argv )
     //drawMatches
 
     drawMatches (img_1, keypoints_1, img_2, keypoints_2, matches, img_match);
+
     imshow ( "result low", img_match );
     imshow ( "result high", img_goodmatch );
     waitKey(0);
-
     return 0;
 }
